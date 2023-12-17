fc47dde74e77ba3de7c7f9ed085df83477f8b913
sepehrshn <46319199+sepehrshn@users.noreply.github.com>
2023-12-17 13:59:53 +0330

    maha_builder

---
+++ sources/SepehrSherian/maha-builder/CommandTemplates/AddPackage.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace CommandTempaltes;
+
+public static class AddPackage
+{
+  public static string Add(string packageName)
+    {
+        if (!string.IsNullOrEmpty(packageName))
+        {
+            var command = new StringBuilder();
+            command.Append($"dotnet add package {packageName}");
+            return command.ToString();
+        }
+        else return "error";
+    }
+
+}
---
+++ sources/SepehrSherian/maha-builder/CommandTemplates/CheckProjectNameValidity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandTemplates
+{
+    public static class CheckProjectNameValidity
+    {
+        public static bool CheckProjectName(string folderName)
+        {
+            if (string.IsNullOrEmpty(folderName))
+            {
+                
+                return false;
+            }
+            return true;
+        }
+        public static bool CheckDircectory(string folderName)
+        {
+            if (Directory.Exists(folderName))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
