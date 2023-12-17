4bb70b07e5a32060efdd9c744249fb141bb1ca97
MJebelli <s.m.jebelli@gmail.com>
2023-12-17 12:18:34 +0330

    maha-builder created

---
+++ sources/MohammadJebelli/CLITemplate/test/Test.CLITemplate/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Test.CLITemplate;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ sources/MohammadJebelli/CLITemplate/test/Test.CLITemplateApi/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Test.CLITemplateApi;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
