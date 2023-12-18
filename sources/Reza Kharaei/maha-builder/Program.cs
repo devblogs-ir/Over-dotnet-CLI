74258b7b5bc72ac1ec4927512a37ec7339f11660
Reza Kharaei <kharaei.developer@gmail.com>
2023-12-18 07:28:18 +0330

    fixed bug (create blank solution)

--- sources/Reza Kharaei/maha-builder/Program.cs
+++ sources/Reza Kharaei/maha-builder/Program.cs
@@ -9,34 +9,39 @@ if (args.Length != 1)
 
 string folderName = args[0];
 
-Directory.CreateDirectory(folderName);
+string directoryPath = Directory.GetCurrentDirectory();
+string ProjectRootDirectory = Path.Combine(directoryPath, folderName);
+     
+Directory.CreateDirectory(ProjectRootDirectory);
 Console.WriteLine($"folder '{folderName}' created.");
 
-Directory.CreateDirectory(folderName + "/Src");
+string srcFolderPath = Path.Combine(ProjectRootDirectory, "src");
+Directory.CreateDirectory(srcFolderPath);
 Console.WriteLine("folder 'Src' created.");
 
-Directory.CreateDirectory(folderName + "/Test");
-Console.WriteLine("folder 'Test' created.");
+string testFolderPath = Path.Combine(ProjectRootDirectory, "test");
+Directory.CreateDirectory(testFolderPath);
+Console.WriteLine("folder 'Test' created."); 
 
-RunDotnetCommand("dotnet new sln DotNetCliExample");
+RunDotnetCommand($"dotnet new sln -n DotNetCliExample --output {ProjectRootDirectory}");
 Console.WriteLine("Blank Solution Created.");
 
-RunDotnetCommand($"dotnet new console -n DotNetCliExample.Console --output {folderName}/Src/DotNetCliExample.Console");
+RunDotnetCommand($"dotnet new console -n DotNetCliExample.Console --output {srcFolderPath}/DotNetCliExample.Console");
 Console.WriteLine("Console Project Created.");
 
-RunDotnetCommand($"dotnet new classlib -n DotNetCliExample.Library --output {folderName}/Src/DotNetCliExample.Library");
+RunDotnetCommand($"dotnet new classlib -n DotNetCliExample.Library --output {srcFolderPath}/DotNetCliExample.Library");
 Console.WriteLine("Class Library Project Created.");
 
-RunDotnetCommand($"dotnet new webapi -n DotNetCliExample.WebApi --output {folderName}/Src/DotNetCliExample.WebApi");
+RunDotnetCommand($"dotnet new webapi -n DotNetCliExample.WebApi --output {srcFolderPath}/DotNetCliExample.WebApi");
 Console.WriteLine("WebApi ProjectsCreated.");
 
-RunDotnetCommand($"dotnet new xunit -n DotNetCliExample.Console.Test --output {folderName}/Test/DotNetCliExample.Console.Test");
+RunDotnetCommand($"dotnet new xunit -n DotNetCliExample.Console.Test --output {testFolderPath}/DotNetCliExample.Console.Test");
 Console.WriteLine("Console.Test Project Created.");
 
-RunDotnetCommand($"dotnet new xunit -n DotNetCliExample.Library.Test --output {folderName}/Test/DotNetCliExample.Library.Test");
+RunDotnetCommand($"dotnet new xunit -n DotNetCliExample.Library.Test --output {testFolderPath}/DotNetCliExample.Library.Test");
 Console.WriteLine("Library.Test Project Created.");
 
-RunDotnetCommand($"dotnet new xunit -n DotNetCliExample.WebApi.Test --output {folderName}/Test/DotNetCliExample.WebApi.Test");
+RunDotnetCommand($"dotnet new xunit -n DotNetCliExample.WebApi.Test --output {testFolderPath}/DotNetCliExample.WebApi.Test");
 Console.WriteLine("WebApi.Test Project Created.");
 
 Console.WriteLine("MahaBuilder completed successfully.");
@@ -44,31 +49,25 @@ Console.WriteLine("MahaBuilder completed successfully.");
 
 // Local Method for Run Dot Net Commands :
 static void RunDotnetCommand(string CliCommandString)
+{
+    var process = new System.Diagnostics.Process
     {
-        var process = new System.Diagnostics.Process
+        StartInfo = new System.Diagnostics.ProcessStartInfo
         {
-            StartInfo = new System.Diagnostics.ProcessStartInfo
-            {
-                FileName = "dotnet",
-                Arguments = CliCommandString,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            }
-        };
+            FileName = "dotnet",
+            Arguments = CliCommandString,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        }
+    };
 
-        process.Start();
-        process.WaitForExit();
+    process.Start();
+    process.WaitForExit();
 
-        string output = process.StandardOutput.ReadToEnd();
-        string error = process.StandardError.ReadToEnd();
-
-        if (!string.IsNullOrWhiteSpace(output))
-            Console.WriteLine(output);
-
-        if (!string.IsNullOrWhiteSpace(error))
-            Console.Error.WriteLine(error);
-    }
-
+    string error = process.StandardError.ReadToEnd();
+    if (!string.IsNullOrWhiteSpace(error))
+        Console.Error.WriteLine(error);
+}
 
